8b000d5355ca64e4b0ef7651b3fb9399c5a2d81a
Grzesiek Macko <grzmacko@gmail.com>
2019-02-28 14:43:56 +0100

    All seeds has been added to one file.

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      $this->call(NoticesTableSeeder::class);
+       $this->call(Sms_alertsTableSeeder::class);
     }
 }
