effaab12ace5eeab52b0b818b189abbd87257365
Grzegorz Macko <grzmacko@gmail.com>
2019-03-04 21:53:46 +0100

    Get all record function has been created.

--- skrypty/skrypt.php
+++ skrypty/skrypt.php
@@ -16,11 +16,21 @@ require('simple_html_dom.php');
 */
 
 $html = file_get_html("https://krosno24.pl/anonse/kategoria/27");
-$info['tytul']	= $html->find(".stre1vda-tre1vda-title",1)->innertext;
-$info['opis']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(1)->children(1)->innertext;
-$info['cena']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(1)->children(0)->children(0)->children(3)->innertext;
-$info['tel']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(2)->children(0)->children(0)->children(2)->innertext;
 
-print_r($info);
+$i = 0;
+
+$info['tytul']	= " ";
+
+$wia['tresc'];
+
+while ($info['tytul'] != "") {
+	$info['tytul']	= $html->find(".stre1vda-tre1vda-title",$i)->innertext;
+	$info['opis']	= $html->find(".stre1vda-tre1vda-title",$i)->parent()->parent()->parent()->children(1)->children(1)->innertext;
+	$info['cena']	= $html->find(".stre1vda-tre1vda-title",$i)->parent()->parent()->parent()->children(1)->children(0)->children(0)->children(3)->innertext;
+	$info['tel']	= $html->find(".stre1vda-tre1vda-title",$i)->parent()->parent()->parent()->children(2)->children(0)->children(0)->children(2)->innertext;
+	$i ++;
+    print_r($info);
+}
+print_r($info['tytul']);
 //echo $html;
 ?>
