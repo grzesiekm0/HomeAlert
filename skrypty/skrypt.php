5456616e7215e6211ecabed89d8aca3702b75b96
Grzegorz Macko <grzmacko@gmail.com>
2019-03-04 21:08:51 +0100

    Script for download site has been created and optimalized.

---
+++ skrypty/skrypt.php
@@ -0,0 +1,26 @@
+<pre><?php
+/*
+	@phpSolutions
+	Kursy video dotyczące zagadnień związanych z PHP
+	
+	Odcinek 01: Pobieranie informacji z innych stron
+*/
+
+require('simple_html_dom.php');
+
+/*
+	Klasa		.nazwaklasy
+	ID		#nazwaid
+	tag		img
+	klasa + name	.nazwaklasy[name='nazwa']
+*/
+
+$html = file_get_html("https://krosno24.pl/anonse/kategoria/27");
+$info['tytul']	= $html->find(".stre1vda-tre1vda-title",1)->innertext;
+$info['opis']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(1)->children(1)->innertext;
+$info['cena']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(1)->children(0)->children(0)->children(3)->innertext;
+$info['tel']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(2)->children(0)->children(0)->children(2)->innertext;
+
+print_r($info);
+//echo $html;
+?>
