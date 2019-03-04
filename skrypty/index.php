0bbae1cf530a2583a236d13181f27cb0595b81de
Grzegorz Macko <grzmacko@gmail.com>
2019-03-04 22:53:28 +0100

    Script to displays rows of database.

---
+++ skrypty/index.php
@@ -0,0 +1,31 @@
+<?php
+ include 'db_connection.php';
+ $conn = OpenCon();
+ echo "Connected Successfully";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $dbname = "home";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+
+$sql = "SELECT home_data_id, description FROM home_data";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["home_data_id"]. " - Name: " . $row["description"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+CloseCon($conn);
+?>
