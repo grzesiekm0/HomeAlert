e93642c1b41ff74262dac1991228d693e4ea7a72
Grzegorz Macko <grzmacko@gmail.com>
2019-03-12 23:39:03 +0100

    Old scrypt has been refactored.

--- skrypty/skrypt.php
+++ skrypty/skrypt.php
@@ -15,14 +15,13 @@ require('simple_html_dom.php');
 	klasa + name	.nazwaklasy[name='nazwa']
 */
 
+
 $html = file_get_html("https://krosno24.pl/anonse/kategoria/27");
 
 $i = 0;
-
 $info['tytul']	= " ";
 
-$des["description"] = "";
-$cars = array();
+$wia['tresc'];
 
 while ($info['tytul'] != "") {
 	$info['tytul']	= $html->find(".stre1vda-tre1vda-title",$i)->innertext;
@@ -30,11 +29,17 @@ while ($info['tytul'] != "") {
 	$info['cena']	= $html->find(".stre1vda-tre1vda-title",$i)->parent()->parent()->parent()->children(1)->children(0)->children(0)->children(3)->innertext;
 	$info['tel']	= $html->find(".stre1vda-tre1vda-title",$i)->parent()->parent()->parent()->children(2)->children(0)->children(0)->children(2)->innertext;
 	$i ++;
-	//$des["description"] =+ $info;
     print_r($info);
-	array_push($cars, $info);
-}
 
-print_r($info['tytul']);
+}
+print_r($info);
+
+
+//$info['tytul']	= $html->find(".stre1vda-tre1vda-title",1)->innertext;
+// $info['opis']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(1)->children(1)->innertext;
+// $info['cena']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(1)->children(0)->children(0)->children(3)->innertext;
+// $info['tel']	= $html->find(".stre1vda-tre1vda-title",1)->parent()->parent()->parent()->children(2)->children(0)->children(0)->children(2)->innertext;
+
+//print_r($info);
 //echo $html;
 ?>
